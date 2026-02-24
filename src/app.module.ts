1201149ff8d62a65aff7840073800889ee326bc7
Jenna428 <jeniffer28nascimento@gmail.com>
2026-02-23 22:45:56 -0300

    fix app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ormConfig } from './config/ormConfig';
 import { UserModule } from './module/user.module';
@@ -15,7 +13,7 @@ import { TransactionModule } from './module/transaction.module';
     TransactionTypeModule,
     TransactionModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
